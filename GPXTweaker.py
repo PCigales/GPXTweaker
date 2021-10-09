c67174772ea0acc11b2d2669cd1d3a8574069403
PCigales <58741513+PCigales@users.noreply.github.com>
2021-10-10 01:14:27 +0200

    Workaround for IGN iti service reversing short pathes

--- GPXTweaker.py
+++ GPXTweaker.py
@@ -2266,7 +2266,8 @@ class WGS84Itinerary(WGS84Map):
       iti = json.loads(rep.body)
       for k in infos['key']:
         iti = iti[k]
-      if sum(map(lambda t:(t[1]-t[0])**2, zip(WGS84WebMercator.WGS84toWebMercator(*iti[0][::-1]), WGS84WebMercator.WGS84toWebMercator(*map(float, points[0]))))) > sum(map(lambda t:(t[1]-t[0])**2, zip(WGS84WebMercator.WGS84toWebMercator(*iti[0][::-1]), WGS84WebMercator.WGS84toWebMercator(*map(float, points[1]))))):
+      if sum(map(lambda t:(t[1]-t[0])**2, zip(WGS84WebMercator.WGS84toWebMercator(*iti[0][::-1]), WGS84WebMercator.WGS84toWebMercator(*map(float, points[0]))))) > sum(map(lambda t:(t[1]-t[0])**2, zip(WGS84WebMercator.WGS84toWebMercator(*iti[-1][::-1]), WGS84WebMercator.WGS84toWebMercator(*map(float, points[0]))))):
+        print("r")
         iti.reverse()
       return list(map(lambda s:s[::-1], iti))
     except:
